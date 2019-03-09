db026643b6749dea1b302f874bbf63e16897c8dc
zhoupengqiang <pengqiangzhou@163.com>
2019-03-09 21:31:10 +0800

    调整配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
     args: 'one two',
     instances: 1,
     autorestart: true,
-    watch: true,
-    ignore_watch: ['node_modules', 'client', 'sql', '.git'],
+    watch: false,
+    // watch 意义不大，通常更新，都是需要重跑 npm install
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
